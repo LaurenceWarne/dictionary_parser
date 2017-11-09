ec49bfc2ab70cfcf30f3f58945dcac3039083cfb
lWarne <laurencewarne@gmail.com>
2017-11-09 19:57:46 +0000

    Create and finish database pipeline and owlbot dictionary spider.

--- dictionary_parser/pipelines.py
+++ dictionary_parser/pipelines.py
@@ -4,8 +4,98 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import json
+import sqlite3
+
+from scrapy.exceptions import DropItem
 
 
-class DictionaryParserPipeline(object):
+class OwlbotDictionaryJlinesPipeline():
+
+    def open_spider(self, spider):
+
+        self.file = open("dict.jl", "w")
+
+    def close_spider(self, spider):
+
+        self.file.close()
+
     def process_item(self, item, spider):
+
+        line = json.dumps(item) + "\n"
+        self.file.write(line)
+        return item
+
+
+class OwlbotDictionaryDatabasePipeline(object):
+
+    def __init__(self, database_name="owlbot_dictionary.db"):
+
+        self.conn = None
+        self.cursor = None
+        self.database_name = database_name
+
+    def open_spider(self, spider):
+
+        self.conn = sqlite3.connect(self.database_name)
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('''
+        CREATE TABLE words(
+        word text PRIMARY KEY
+        )''')
+        self.cursor.execute('''
+        CREATE TABLE definitions(
+        word text,
+        type text,
+        definition text,
+        PRIMARY KEY(word, definition),
+        FOREIGN KEY(word) REFERENCES words(word)
+        )''')
+        self.cursor.execute('''
+        CREATE TABLE examples(
+        word text,
+        definition text,
+        example text,
+        PRIMARY KEY(word, definition, example),
+        FOREIGN KEY(word) REFERENCES words(word),
+        FOREIGN KEY(definition) REFERENCES definitions(definition)
+        )''')
+
+    def close_spider(self, spider):
+
+        self.conn.commit()
+        self.conn.close()
+
+    def process_item(self, item, spider):
+
+        if (item['word'] is None):
+            raise DropItem('Dictionary entry missing name')
+        elif (item['definition'] is None):
+            raise DropItem(
+                'Dictionary entry ' + item['word'] + ' is missing a definition'
+            )
+        elif (item['type'] is None):
+            raise DropItem(
+                'Dictionary entry ' + item['word'] + ' is missing a word-type'
+            )
+
+        
+        self.cursor.execute(
+            'SELECT word FROM words WHERE word=?', (item['word'],)
+        )
+        if (self.cursor.fetchall() == []):
+            self.cursor.execute(
+                '''INSERT INTO words VALUES(?)''', (item["word"],)
+            )
+
+        
+        self.cursor.execute(
+            '''INSERT INTO definitions VALUES(?, ?, ?)''',
+            (item['word'], item['type'], item['definition'])
+        )
+        if (not item['example'] is None):
+            self.cursor.execute(
+                '''INSERT INTO examples VALUES(?, ?, ?)''',
+                (item['word'], item['definition'], item['example'])
+            )
         return item
---
+++ dictionary_parser/spiders/owlbot_dictionary_spider.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import json
+import re
+
+import scrapy
+
+
+class OwlbotDictionarySpider(scrapy.Spider):
+
+    name = 'owlbot_dictionary_spider'
+    api_url = "https://owlbot.info/api/v2/dictionary/"
+    # slice to obtain the word from the response url
+    word_slice = slice(len(api_url), -len("?format=json") - 3)
+    allowed_domains = ['owlbot.info']
+    # regex to remove tags which are in the text body for some reason.
+    regex = re.compile("<.*>?")
+
+    def __init__(self, wordfile=None, verbose=True):
+
+        self.wordfile = wordfile
+        if (self.wordfile is None):
+            raise ValueError("Input file of words must be passed!")
+        self.verbose = verbose
+
+    def start_requests(self):
+
+        with open(self.wordfile, mode="r") as f:
+            words = f.readlines()
+        for word in words:
+            yield scrapy.Request(
+                self.api_url + word + "?format=json", self.parse
+            )
+
+    def parse(self, response):
+
+        data_dictionary = json.loads(response.text)
+        word = response.url[self.word_slice]
+        # Iterate over defintions of the word
+        if (data_dictionary == []):
+            if (self.verbose):
+                self.logger.error("No defintions found for: " + word)
+            return
+        for diff_defintion in data_dictionary:
+            # Iterate over information about a particular defintion
+            for key, val in diff_defintion.items():
+                try:
+                    new_val = self.regex.sub("", val)
+                except TypeError:  # null, e.g. no example given
+                    # We'll log these in the pipeline
+                    pass
+                else:
+                    diff_defintion[key] = new_val
+            diff_defintion["word"] = word
+            yield diff_defintion
